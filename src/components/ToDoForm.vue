7bf04c3b34a20c3ba8faf6bb08530b4c921ce72c
Leah Meromy <lmeromy@gmail.com>
2023-01-26 10:20:56 -0700

    adds styles and counter

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,13 +1,18 @@
 <template>
     <form @submit.prevent="onSubmit">
-    <label for="new-todo-input"> What needs to be done? </label>
-    <input
+        <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg">
+        What needs to be done?
+      </label>
+    </h2>
+     <input
       type="text"
       id="new-todo-input"
       name="new-todo"
       autocomplete="off"
-      v-model.lazy.trim="label" />
-    <button type="submit">Add</button>
+      v-model.lazy.trim="label"
+      class="input__lg" />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
   </form>
 </template>
 
@@ -19,6 +24,7 @@
     return;
   }
             this.$emit("todo-added", this.label);
+            this.label = "";
         }
     },
     data(){
